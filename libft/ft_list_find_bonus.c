50b1b38e5b33e9d438850a3c5fb9e432bd6a106b
Felipe Coelho <fecoel@hotmail.com>
2020-10-24 17:20:54 +0200

    norm

--- libft/ft_list_find_bonus.c
+++ libft/ft_list_find_bonus.c
@@ -1,17 +1,13 @@
-//
-// Created by fecoe on 23/10/2020.
-//
-
 #include "libft.h"
 
 t_list	*ft_list_find(t_list *begin_list, void *data_ref,
-                        size_t n, int (*cmp)())
+		size_t n, int (*cmp)())
 {
-    while (begin_list)
-    {
-        if (!(*cmp)(begin_list->content, data_ref, n))
-            return (begin_list);
-        begin_list = begin_list->next;
-    }
-    return (begin_list);
+	while (begin_list)
+	{
+		if (!(*cmp)(begin_list->content, data_ref, n))
+			return (begin_list);
+		begin_list = begin_list->next;
+	}
+	return (begin_list);
 }
